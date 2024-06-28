842f5a86f945b77ef0d08babb91fe4d3141af724
JabedHossainSwe <jabedhossain.swe@gmail.com>
2024-06-28 22:28:39 +0600

    "Refactor image index view to use Bootstrap card layout and update styling"

--- resources/views/images/index.blade.php
+++ resources/views/images/index.blade.php
@@ -1,19 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Images</h1>
-    <a href="{{ route('images.create') }}">Upload New Images</a>
-    <ul>
+    <h1>Uploaded Images</h1>
+    <div class="row">
         @foreach ($images as $image)
-            <li>
-                <img src="{{ asset('storage/' . $image->file_path) }}" alt="{{ $image->file_name }}" width="100">
-                <a href="{{ route('images.edit', $image) }}">Edit</a>
-                <form action="{{ route('images.destroy', $image) }}" method="POST" style="display:inline;">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit">Delete</button>
-                </form>
-            </li>
+            <div class="col-md-3">
+                <div class="card mb-4 shadow-sm">
+                    <img src="{{ asset('storage/' . $image->file_path) }}" alt="{{ $image->file_name }}" class="card-img-top"
+                        width="100">
+                    <div class="card-body">
+                        <p class="card-text">{{ $image->file_name }}</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <a href="{{ route('images.edit', $image) }}"
+                                    class="btn btn-sm btn-outline-secondary">Edit</a>
+                                <form action="{{ route('images.destroy', $image) }}" method="POST" class="d-inline-block">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-sm btn-outline-danger">Delete</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         @endforeach
-    </ul>
+    </div>
 @endsection
